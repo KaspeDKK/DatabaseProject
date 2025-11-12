9079ce613b8df76f05b94ab901b0873d80358ba0
SimoXSwagger <88721973+SimoXSwagger@users.noreply.github.com>
2025-11-12 15:14:49 +0100

    Update SQL_Data_Queries.sql

--- SQL_Data_Queries.sql
+++ SQL_Data_Queries.sql
@@ -6,7 +6,8 @@ HAVING COUNT(customerCPR)>1;
 SELECT DISTINCT part_code, manufacturer_ID FROM Parts NATURAL JOIN Manufacturer
 WHERE part_code NOT IN (SELECT part_code FROM Uses);
 -- For each part, show the code, manufacturer, and total quantity being used for all repair jobs in 2024
-
+SELECT part_code, manufacturer_ID, sum(quantity) AS total FROM RepairJobs NATURAL JOIN Uses NATURAL JOIN parts
+WHERE repair_date LIKE '2024%' GROUP BY part_code;
 -- For each bike type, show the type, code and manufacturer of the most repaired bike. (NEEDS TO BE CHECKED)
 DROP VIEW IF EXISTS BikeRepairs;
 CREATE VIEW BikeRepairs(bike_code, repairs_count) AS
