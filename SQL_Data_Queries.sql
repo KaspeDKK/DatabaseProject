8d2b89f5b4d838ed62d9a17f570ce2aaffa48402
SimoXSwagger <88721973+SimoXSwagger@users.noreply.github.com>
2025-11-12 14:59:05 +0100

    Update SQL_Data_Queries.sql

--- SQL_Data_Queries.sql
+++ SQL_Data_Queries.sql
@@ -3,7 +3,8 @@ SELECT customerCPR FROM RepairJobs
 GROUP BY customerCPR
 HAVING COUNT(customerCPR)>1;
 -- Show the code and manufacturer of all parts that were never used for any repair
-
+SELECT DISTINCT part_code, manufacturer_ID FROM Parts NATURAL JOIN Manufacturer
+WHERE part_code NOT IN (SELECT part_code FROM Uses);
 -- For each part, show the code, manufacturer, and total quantity being used for all repair jobs in 2024
 
 -- For each bike type, show the type, code and manufacturer of the most repaired bike.
